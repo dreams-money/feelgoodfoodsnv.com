3bc15f0bab5a472f52c1f3656bd1e509f8a309bd
Cesar V <cesar.binary@gmail.com>
2023-08-25 18:59:29 -0700

    Optimized web responsiveness and asset preloads

--- web-root/assets/scripts/pickup.js
+++ web-root/assets/scripts/pickup.js
@@ -3,7 +3,3 @@ map.addEventListener('load', ()=>{
     document.getElementById('loading-spinner').style.display = 'none';
     map.style.display = 'block';
 });
-
-document.getElementById('address').addEventListener('click', () => {
-    window.location = 'https://www.google.com/maps/dir//3922%20US-50,%20Carson%20City,%20NV%2089701';
-});
